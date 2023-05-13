86c51cc196ca07f35d1038490b69589808a5f9ed
werty144 <anton-paramonov2000@yandex.ru>
2023-05-13 16:05:54 +0200

    working

--- src/main/kotlin/com/example/internship_plugin/CalendarToolWindowFactory.kt
+++ src/main/kotlin/com/example/internship_plugin/CalendarToolWindowFactory.kt
@@ -1,22 +1,13 @@
 package com.example.internship_plugin
 
 
-import com.intellij.ide.highlighter.JavaFileType
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.AnActionResult
-import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.openapi.vfs.newvfs.BulkFileListener
-import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.psi.PsiManager
-import com.intellij.psi.PsiTreeChangeEvent
-import com.intellij.psi.PsiTreeChangeListener
 import com.intellij.psi.impl.source.PsiClassImpl
+import com.intellij.psi.impl.source.PsiMethodImpl
 import com.intellij.psi.search.FilenameIndex
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.PsiModificationTracker
@@ -24,10 +15,8 @@ import com.intellij.psi.util.childrenOfType
 import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
 import javax.swing.BoxLayout
-import javax.swing.JFrame
 import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.OverlayLayout
 
 
 class CalendarToolWindowFactory : ToolWindowFactory {
@@ -39,33 +28,38 @@ class CalendarToolWindowFactory : ToolWindowFactory {
         project.messageBus.connect().subscribe(PsiModificationTracker.TOPIC, MyPsiListener(project, this))
     }
 
+    class FileClassesFunctions(val file: String, val classes: Int, val functions: Int)
+
     internal class MyPsiListener(val project: Project,
                                     val calendarToolWindowFactory: CalendarToolWindowFactory) : PsiModificationTracker.Listener {
         init {
-            val classes = collectClasses()
-            calendarToolWindowFactory.redrawContent(classes)
+            val data = collectData()
+            calendarToolWindowFactory.redrawContent(data)
         }
 
-        private fun classesFromFile(file: VirtualFile): List<PsiClassImpl> {
+        private fun classesInFile(file: VirtualFile): List<PsiClassImpl> {
             return PsiManager.getInstance(project).findFile(file)!!.childrenOfType<PsiClassImpl>()
         }
 
-        fun collectClasses(): List<PsiClassImpl> {
+        fun collectData(): List<FileClassesFunctions> {
             val files = FilenameIndex.getAllFilesByExt(project, "java", GlobalSearchScope.projectScope(project))
-            return files.mapNotNull { file ->  classesFromFile(file)}.flatten()
+            return files.mapNotNull { file ->
+                val classes = classesInFile(file)
+                FileClassesFunctions(file.name, classes.size, classes.sumOf { it.methods.size })
+            }
         }
 
         override fun modificationCountChanged() {
-            val classes = collectClasses()
-            calendarToolWindowFactory.redrawContent(classes)
+            val data = collectData()
+            calendarToolWindowFactory.redrawContent(data)
         }
     }
 
-    fun redrawContent(classes: List<PsiClassImpl>) {
+    fun redrawContent(fileClassesFunctions: List<FileClassesFunctions>) {
         toolWindowContent.contentPanel.removeAll()
-        classes.forEach {
+        fileClassesFunctions.forEach {
             val label = JLabel()
-            label.text = it.name
+            label.text = "${it.file}: ${it.classes} classes; ${it.functions} functions"
             toolWindowContent.contentPanel.add(label)
         }
         toolWindowContent.contentPanel.revalidate()
